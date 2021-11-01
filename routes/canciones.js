46f850f93e188bd5340679f98e7efeac09366a03
sahuer <sahuer.daniel@gmail.com>
2021-11-01 02:05:15 -0600

    Actualizacion Models/Routes

--- routes/canciones.js
+++ routes/canciones.js
@@ -1,11 +1,10 @@
 // Bibliotecas
-const express = require('express')
-const cController = require('../controllers/canciones')
-const router = express.Router()
+const router = require('express').Router();
+const cController = require('../controllers/canciones');
 
 router.post('/agregarCancion',cController.postAgregarCancion)
 router.get('/obtenerCancion',cController.getObtenerCancion)
 router.post('/borrarCancion',cController.postBorrarCancion)
-router.post('/actualizarCancion'.cController.postActualizarCancion)
+router.post('/actualizarCancion',cController.postActualizarCancion)
 
 module.exports = router
--- routes/libros.js
+++ routes/libros.js
@@ -1,11 +1,10 @@
 // Bibliotecas
-const express = require('express')
-const lController = require('../controllers/libros')
-const router = express.Router()
+const router = require('express').Router();
+const lController = require('../controllers/libros');
 
 router.post('/agregarLibro',lController.postAgregarLibro)
 router.get('/obtenerLibro',lController.getObtenerLibro)
 router.post('/borrarLibro',lController.postBorrarLibro)
-router.post('/actualizarLibro'.lController.postActualizarLibro)
+router.post('/actualizarLibro',lController.postActualizarLibro)
 
 module.exports = router
