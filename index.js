17555e75c13d2c96c1033bbc228f778422bdf6a6
sahuer <sahuer.daniel@gmail.com>
2021-11-01 02:05:37 -0600

    Actualizacion Index

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended:true}))
 app.use('/libros',librosRoutes)
 app.use('/canciones',cancionesRoutes)
 
-mongoose.connect('mongodb://userX:root@54.198.161.35:27017/baseX?authSource=admin')
+mongoose.connect('mongodb://user13:root@54.198.161.35:27017/base13?authSource=admin')
 .then(()=>{
     app.listen(8084,()=>console.log('Servidor en linea en puerto 8084'))
 })
